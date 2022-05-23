0db5fedefafb9864dfa4bd40a5f66f628a8fc0d8
Kestutis <kestutis.julius@gmail.com>
2022-05-23 13:13:09 +0300

    Film API initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import { useEffect } from "react";
+
 import './App.css';
+const API_URL = 'http://www.omdbapi.com?apikey=cd163564';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+        console.log(data.Search);
+    }
+
+    useEffect(()=>{
+        searchMovies('SpiderMan');
+    }, []);
+    return(
+
+            <h1>App</h1>
+    );
 }
-
 export default App;
